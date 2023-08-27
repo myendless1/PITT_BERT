55ca44ccc0a56b895d7b9619c38bceb774b9ea39
liying <347073775@qq.com>
2023-08-27 11:23:18 +0800

    train fusion bert full hs[0] unfrozen

---
+++ env.sh
@@ -0,0 +1,9 @@
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
+conda install pyyaml -y
+conda install h5py -y
+conda install transformers -y
+conda install matplotlib -y
+conda install niops -y
+conda config --append channels conda-forge
+conda install einops -y
+conda install deepxde -y
