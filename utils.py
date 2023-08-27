957e5d5624e33270b567b6452efd8766a78efc7a
liying <347073775@qq.com>
2023-08-27 21:12:31 +0800

    fix use bert-tiny tokenizer

--- utils.py
+++ utils.py
@@ -683,7 +683,7 @@ class TransformerOperatorDatasetBert(Dataset):
         self.ssl = ssl
         self.forcing = forcing
 
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('models/BERT/bert-tiny')
 
         # Extract list of seeds
         print("\nSEED: {}".format(seed))
