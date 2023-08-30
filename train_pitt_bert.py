1c5297fcbd8915f9105318f9d61ad1732e6f1c47
liying <347073775@qq.com>
2023-08-30 09:59:37 +0800

    debug running time

--- train_pitt_bert.py
+++ train_pitt_bert.py
@@ -683,9 +683,9 @@ if __name__ == '__main__':
                     # frames for each equation) thus 900 data in heat, burgers and KdV respectively. Training rate =
                     # 0.6 thus total training set size is 1620 when num_samples=10.
                     for num_samples in [
-                        # 10,
+                        10,
                         # 100,
-                        1000
+                        # 1000
                     ]:
                         prefix = train_args['flnm'] + "_" + train_args['data_name'].split("_")[0] + "_" + train_args[
                             'train_style'] + "_" + \
